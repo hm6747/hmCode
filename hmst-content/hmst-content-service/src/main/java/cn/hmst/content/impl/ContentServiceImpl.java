1a0c6d959a38b9d81e8a492dac0cfd3986657613
hm <674703614@qq.com>
2017-12-13 18:10:43 +0800

    twetwe

--- hmst-content/hmst-content-service/src/main/java/cn/hmst/content/impl/ContentServiceImpl.java
+++ hmst-content/hmst-content-service/src/main/java/cn/hmst/content/impl/ContentServiceImpl.java
@@ -1,11 +1,15 @@
 package cn.hmst.content.impl;
 
+import cn.hmst.common.jedis.JedisClient;
 import cn.hmst.common.pojo.HmResult;
+import cn.hmst.common.util.JsonUtils;
 import cn.hmst.content.service.ContentService;
 import cn.hmst.mapper.TbContentMapper;
 import cn.hmst.pojo.TbContent;
 import cn.hmst.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 import java.util.List;
@@ -16,6 +20,10 @@ import java.util.List;
 public class ContentServiceImpl implements ContentService{
     @Autowired
     private  TbContentMapper tbContentMapper;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${CONTENT_LIST}}")
+    private  String CONTENT_LIST;
     @Override
     public HmResult addContent(TbContent content) {
         content.setCreated(new Date());
@@ -26,10 +34,24 @@ public class ContentServiceImpl implements ContentService{
 
     @Override
     public List<TbContent> getContentListByCid(long cid) {
+        try {
+            String json = jedisClient.hget(CONTENT_LIST,cid+"");
+            if(StringUtils.isNotBlank(json)){
+                List<TbContent> tbContents =JsonUtils.jsonToList(json,TbContent.class);
+                return  tbContents;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
         List<TbContent> tbContents =tbContentMapper.selectByExampleWithBLOBs(example);
+        try {
+            jedisClient.hset(CONTENT_LIST,cid+"", JsonUtils.objectToJson(tbContents));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return tbContents;
     }
 }
