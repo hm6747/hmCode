328f57cfa1f897b7fd5512911e17f2dcc3709184
hm <674703614@qq.com>
2017-12-20 20:21:37 +0800

    twetwe

---
+++ hmst-search/hmst-search-service/src/main/java/cn/hmst/search/mapper/ItemMapper.java
@@ -0,0 +1,12 @@
+package cn.hmst.search.mapper;
+
+import cn.hmst.common.pojo.SearchItem;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/12/20 0020.
+ */
+public interface ItemMapper {
+    List<SearchItem> getItemList();
+}
