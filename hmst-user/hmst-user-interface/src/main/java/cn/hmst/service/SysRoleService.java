2c5776d3644956a876d80c662ffdf970817a203e
hm6747 <674703614@qq.com>
2017-12-26 15:25:54 +0800

    twetwe

--- hmst-user/hmst-user-interface/src/main/java/cn/hmst/service/SysRoleService.java
+++ hmst-user/hmst-user-interface/src/main/java/cn/hmst/service/SysRoleService.java
@@ -1,6 +1,9 @@
 package cn.hmst.service;
 
 import cn.hmst.param.RoleParam;
+import cn.hmst.pojo.SysRole;
+
+import java.util.List;
 
 /**
  * Created by hm on 2017/12/27.
@@ -8,6 +11,7 @@ import cn.hmst.param.RoleParam;
 public interface SysRoleService {
     public void save(RoleParam param);
     public void update(RoleParam param);
+    public List<SysRole> getAll();
 /*    public List<SysRole> getRoleListByUserId(int userId);
     public List<SysRole> getRoleListByAclId(int aclId);
     public List<SysUser> getUserListByRoleList(List<SysRole> roleList);*/
