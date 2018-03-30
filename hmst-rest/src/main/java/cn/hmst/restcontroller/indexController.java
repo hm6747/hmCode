6b331b0b64a0418c95704993adad86e5eb8a2e7a
hm6747 <674703614@qq.com>
2018-03-30 13:59:26 +0800

    Revert: ignored

--- hmst-rest/src/main/java/cn/hmst/restcontroller/indexController.java
+++ hmst-rest/src/main/java/cn/hmst/restcontroller/indexController.java
@@ -1,29 +1,24 @@
 package cn.hmst.restcontroller;
 
-import cn.hmst.content.service.ContentService;
-import cn.hmst.pojo.TbContent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2017/12/4 0004.
  */
 @Controller
 public class indexController {
-    @Autowired
-    private ContentService contentService;
+/*    @Autowired
+    private ContentService contentService;*/
     @Value("${CONTENT_LUNBO_ID}")
     private Long CONTENT_LUNBO_ID;
 
-    @RequestMapping("/index")
+    @RequestMapping("/index.page")
     public String showIndex(Model model){
-        List<TbContent> ad1List = contentService.getContentListByCid(CONTENT_LUNBO_ID);
-        model.addAttribute("ad1List",ad1List);
-        return "index";
+//        List<TbContent> ad1List = contentService.getContentListByCid(CONTENT_LUNBO_ID);
+//        model.addAttribute("ad1List",ad1List);
+        return  "redirect:/pages/index.html";
     }
 }
